81a4b40c9ce2bab150901884fb9f1df2bc682813
3NiBhavnagar <3nibvntester31020@gmail.com>
2025-01-01 12:03:48 +0530

    Set Auction data in front of today's auction date

--- app/Http/Controllers/AuctionController.php
+++ app/Http/Controllers/AuctionController.php
@@ -8,9 +8,20 @@ use Illuminate\Http\Request;
 
 class AuctionController extends Controller
 {
+    // public function showAuctions()
+    // {
+    //     $auctions = Auction::with('players')->take(3)->get();
+    //     return view('index', compact('auctions'));
+    // }
+
     public function showAuctions()
     {
-        $auctions = Auction::with('players')->take(3)->get();
+        $today = now()->toDateString(); // Get today's date in 'Y-m-d' format
+        $auctions = Auction::whereDate('auction_date', $today) // Filter by today's date
+            ->with('players')
+            ->take(3)
+            ->get();
+
         return view('index', compact('auctions'));
     }
 }
